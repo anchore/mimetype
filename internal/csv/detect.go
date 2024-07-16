60d8206f3afbd4394a5013958eb41f3ccc063894
Alex Goodman <wagoodman@users.noreply.github.com>
2024-07-16 08:20:49 -0400

    refactor to sliding window approach
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- internal/csv/detect.go
+++ internal/csv/detect.go
@@ -2,6 +2,8 @@ package csv
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"github.com/gabriel-vasile/mimetype/internal/util"
 	"io"
 )
@@ -12,79 +14,418 @@ const (
 	comment     = '#'
 )
 
+type detectState struct {
+	// source
+	delimiter byte
+	lineLimit int
+
+	// read state
+	prev                                                                                                                     *byte
+	cur                                                                                                                      byte
+	next                                                                                                                     *byte
+	lineSize, csvLineIdx, quoteCount                                                                                         int
+	sawCsvDataOnCurrentLine, isWithinInferredQuote, isWithinExplicitQuote, isWithinComment, nextIsFieldTerminator, isNewline bool
+	recordFields                                                                                                             map[int]int
+
+	// conclusion
+	complete bool
+	invalid  bool
+}
+
+type slidingWindow struct {
+	reader     io.Reader
+	bufSize    int
+	lookAhead  int
+	lookBehind int
+	buf        []byte
+	slide      []byte
+	lastByte   byte
+	firstIter  bool
+}
+
+func newSlidingWindow(reader io.Reader, bufSize, lookAhead, lookBehind int) *slidingWindow {
+	if lookAhead <= 0 {
+		lookAhead = 3
+	}
+	if lookBehind <= 0 {
+		lookBehind = 1
+	}
+	return &slidingWindow{
+		reader:     reader,
+		bufSize:    bufSize,
+		lookAhead:  lookAhead,
+		lookBehind: lookBehind,
+		buf:        make([]byte, bufSize),
+		slide:      make([]byte, 0, lookAhead+lookBehind),
+		firstIter:  true,
+	}
+}
+
+func (sw *slidingWindow) Process(processFunc func(buf []byte, i, length int) int) error {
+
+	var lastOffset int
+	for {
+		n, err := sw.reader.Read(sw.buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return err
+		}
+
+		var combinedBuf []byte
+		var i int
+		if sw.firstIter {
+			combinedBuf = append(combinedBuf, sw.slide...)
+			combinedBuf = append(combinedBuf, sw.buf[:n]...)
+			sw.firstIter = false
+		} else {
+			combinedBuf = append([]byte{sw.lastByte}, sw.slide...)
+			combinedBuf = append(combinedBuf, sw.buf[:n]...)
+			i = 1
+		}
+
+		before := i
+		for {
+			if i >= len(combinedBuf)-sw.lookAhead {
+				break
+			}
+			before = i
+			i = processFunc(combinedBuf, i, len(combinedBuf))
+			lastOffset = i - before
+			i++
+		}
+
+		if len(combinedBuf) > sw.lookAhead {
+
+			sw.lastByte = combinedBuf[before+lastOffset]
+			sw.slide = combinedBuf[before+lastOffset+1:]
+
+		} else {
+			sw.slide = combinedBuf[lastOffset:]
+		}
+	}
+
+	if len(sw.slide) > 0 {
+		finalBuf := append([]byte{sw.lastByte}, sw.slide...)
+		i := sw.lookBehind
+		for {
+			if i >= len(finalBuf) {
+				break
+			}
+			i = processFunc(finalBuf, i, len(finalBuf))
+			i++
+		}
+	}
+
+	return nil
+}
+
 // Detect takes raw bytes and indicates if it is a CSV file (or other given value-delimited file). This reads up
 // to the given limit of bytes to make a determination, validating no further than the first 10 lines of the file.
 func Detect(raw []byte, delimiter byte, limit uint32) bool {
+	lineLimit := svLineLimit
+	if limit > 0 {
+		lineLimit = -1
+	}
 	reader := prepSvReader(raw, limit)
+	state := newDetectState(delimiter, lineLimit)
+	window := newSlidingWindow(reader, 1024, 3, 1)
 
-	isWithinQuote := false
-	isWithinComment := false
-	lineIdx := 0
-	recordFields := make(map[int]int)
+	if err := window.Process(state.read); err != nil {
+		panic("errg")
+		return false
+	}
 
-	buf := make([]byte, 1024)
-	n, err := reader.Read(buf)
+	// treat rows with missing newline as valid lines
+	state.resetLine()
 
-	var prev, cur, next byte
-loop:
-	for err == nil {
-		for i := 0; i < n; i++ {
-			cur = buf[i]
+	return state.isValidCSV()
+}
 
-			if i > 0 {
-				prev = buf[i-1]
+func newDetectState(delimiter byte, lineLimit int) *detectState {
+	return &detectState{
+		delimiter:    delimiter,
+		lineLimit:    lineLimit,
+		recordFields: make(map[int]int),
+	}
+}
+
+func byteStr(ref *byte) string {
+	if ref == nil {
+		return "nil "
+	}
+	b := string(*ref)
+	if b == "\"" {
+		return ` "  `
+	}
+	return fmt.Sprintf("%4s", fmt.Sprintf("%q", b))
+}
+
+func (d *detectState) read(buf []byte, i, n int) int {
+	if d.complete {
+		return i
+	}
+
+	d.cur = buf[i]
+
+	if i > 0 {
+		d.prev = &buf[i-1]
+	} else {
+		d.prev = nil
+	}
+
+	if i < n-1 {
+		d.next = &buf[i+1]
+	} else {
+		d.next = nil
+	}
+
+	isNoNext := d.next == nil
+
+	{
+		var nextNext *byte
+		if i < n-2 {
+			nextNext = &buf[i+2]
+		} else {
+			nextNext = nil
+		}
+
+		isNextLinuxNewline := isByte(d.next, '\n') && !isByte(nextNext, '\r')
+		isNextWindowsNewline := isByte(d.next, '\r') && isByte(nextNext, '\n')
+		isNextDelimiter := isByte(d.next, d.delimiter)
+		d.nextIsFieldTerminator = isNextLinuxNewline || isNextWindowsNewline || isNextDelimiter || isNoNext
+	}
+
+	isLinuxNewline := d.cur == '\n' && !isByte(d.prev, '\r')
+	isWindowsNewline := d.cur == '\r' && isByte(d.next, '\n')
+	d.isNewline = isLinuxNewline || isWindowsNewline
+
+	// edge case from stdlib csv reader: drop trailing carriage returns
+	if d.cur == '\r' && isByte(d.prev, '\n') && isNoNext {
+		// skip processing the trailing carriage return
+		return i
+	}
+
+	if !d.isNewline {
+		d.lineSize++
+	} else {
+		if isWindowsNewline {
+			i++ // don't process \n if we're on the \r
+		}
+		d.handleNewline()
+		return i
+	}
+
+	return d.processLineChar(i)
+}
+
+func isByte(b *byte, c byte) bool {
+	if b == nil {
+		return false
+	}
+	return *b == c
+}
+
+func (d *detectState) handleNewline() {
+	if d.isWithinExplicitQuote {
+		// newlines within quotes are valid
+		return
+	}
+	if d.lineLimit > 0 && d.csvLineIdx >= d.lineLimit {
+		// we've processed as much data as we're allowed to consider
+		d.markComplete()
+		return
+	}
+
+	if !d.isWithinComment && d.lineSize > 0 && !d.lineHasData() {
+		// this should have been a csv line, but we saw content without a delimiter that was not in a comment
+		d.markInvalid()
+		return
+	}
+
+	// iterate to next line...
+	d.resetLine()
+	return
+}
+
+func (d *detectState) processLineChar(i int) int {
+	switch {
+	case d.cur == quote:
+		i = d.handleQuote(i)
+
+	case !d.isWithinComment:
+		d.handleDataCharacter()
+	}
+	return i
+}
+
+func (d *detectState) handleQuote(i int) int {
+	if d.isWithinComment {
+		return i
+	}
+
+	d.startDataLine()
+
+	if d.isWithinExplicitQuote {
+		// we MIGHT be ending a quote...
+		switch {
+		case isByte(d.next, quote):
+			// ... NOPE, this is an escape for the next quote
+			i++ // skip processing the next quote character altogether
+			return i
+		default:
+			if d.nextIsFieldTerminator {
+				// we're ending the quote
+				d.isWithinExplicitQuote = false
+				d.quoteCount++ // count the discovered quote
 			} else {
-				prev = byte(0)
-			}
-
-			if i < n-1 {
-				next = buf[i+1]
-			} else {
-				next = byte(0)
-			}
-
-			isNewline := cur == '\n' && prev != '\r' && next != byte(0) && next != '\n' || cur == '\r'
-
-			switch {
-			case cur == quote:
-				if (!isWithinQuote || next != quote) && !isWithinComment {
-					isWithinQuote = !isWithinQuote
-				} else {
-					i++
-				}
-
-			case isNewline && !isWithinQuote:
-				if lineIdx >= svLineLimit {
-					break loop
-				}
-				_, ok := recordFields[lineIdx]
-				if !isWithinComment && !ok {
-					// this should have been a csv line, but we saw content without a delimiter that was not in a comment
-					return false
-				}
-				lineIdx++
-				isWithinComment = false
-
-			case !isWithinQuote && !isWithinComment:
-				switch cur {
-				case comment:
-					isWithinComment = true
-
-				case delimiter:
-					if recordFields[lineIdx] == 0 {
-						recordFields[lineIdx] = 1
-					}
-					recordFields[lineIdx]++
-				}
+				// this doesn't appear to be the end of a field... so we'll treat it as if this current
+				// quote was escaped
+				//d.quoteCount++ // count the discovered quote
+				return i
 			}
 
 		}
+	} else {
+		// we're within an inferred quote
+		if d.isWithinInferredQuote {
+			if isByte(d.next, d.delimiter) {
+				// we're ending the inferred quote
+				d.isWithinInferredQuote = false
+				d.quoteCount++ // count the inferred quote
+				d.quoteCount++ // count the discovered quote
+			}
+			// we're escaping this quote (don't count it)
+		} else {
+			// we're starting a quote
+			d.isWithinExplicitQuote = true
+			d.isWithinInferredQuote = false
+			d.quoteCount++ // count the discovered quote
+		}
+	}
 
-		n, err = reader.Read(buf)
+	//if !d.isWithinExplicitQuote || d.next != quote {
+	//	// either starting or ending a quote
+	//	d.isWithinExplicitQuote = !d.isWithinExplicitQuote
+	//}
+
+	if d.isWithinExplicitQuote || d.isWithinInferredQuote {
+		return i
+	}
+
+	// quotes should either encapsulate a field entirely or there be only a single quote within the field
+	switch {
+	case d.nextIsFieldTerminator:
+	default:
+		// we found a field that the quote encapsulation is not correct (e.g. ...,"something"else,... )
+		d.markInvalid()
+		return i
+	}
+	return i
+}
+
+func (d *detectState) markInvalid() {
+	d.complete = true
+	d.invalid = true
+}
+
+func (d *detectState) markComplete() {
+	d.complete = true
+}
+
+func (d detectState) lineHasData() bool {
+	_, ok := d.recordFields[d.csvLineIdx]
+	return ok
+}
+
+func (d *detectState) resetLine() {
+	d.isWithinInferredQuote = false
+	d.quoteCount = 0
+
+	if d.sawCsvDataOnCurrentLine {
+		d.csvLineIdx++
+	}
+
+	d.sawCsvDataOnCurrentLine = false
+	d.lineSize = 0
+	d.isWithinComment = false
+}
+
+func (d *detectState) handleDataCharacter() {
+	switch {
+	case d.cur == comment && !d.isWithinExplicitQuote && !d.isWithinInferredQuote:
+		d.isWithinComment = true
+
+	case d.cur == d.delimiter:
+		if !d.isWithinExplicitQuote {
+			d.newField()
+		}
+	default:
+		// we've seen a non-delimiter, so we know this is a data row... but we can't count this as a field until we see the first delimiter
+		if d.startDataLine() {
+			if d.cur != quote {
+				d.isWithinInferredQuote = true
+			}
+		}
+	}
+}
+
+func (d *detectState) newField() {
+	d.isWithinInferredQuote = false
+
+	// 0: no quotes
+	// 1: imbalanced quotes (this condition)
+	// 2: balanced quotes
+	if d.quoteCount == 1 {
+		if !d.isWithinInferredQuote {
+			d.markInvalid()
+		}
+	}
+
+	d.quoteCount = 0
+
+	d.incrementFields()
+	if !d.isWithinInferredQuote {
+		if !isByte(d.next, quote) && !d.nextIsFieldTerminator {
+			// infer that we're starting with data and it's implicitly quoted (lazy quote)
+			d.isWithinInferredQuote = true
+		}
+	}
+}
+
+func (d *detectState) incrementFields() {
+	if d.recordFields[d.csvLineIdx] == 0 {
+		// by definition, we've seen a delimiter, so we can count the previous field and the new field to the right
+		// of the delimiter
+		d.recordFields[d.csvLineIdx] = 1
+	}
+	d.recordFields[d.csvLineIdx]++
+	if !d.sawCsvDataOnCurrentLine {
+		d.sawCsvDataOnCurrentLine = true
+	}
+}
+
+func (d *detectState) startDataLine() bool {
+	var isNew bool
+	if _, ok := d.recordFields[d.csvLineIdx]; !ok {
+		// we've seen at least a single character that is not in a comment, this is a CSV row candidate
+		d.recordFields[d.csvLineIdx] = 0
+		isNew = true
+	}
+	if !d.sawCsvDataOnCurrentLine {
+		d.sawCsvDataOnCurrentLine = true
+	}
+	return isNew
+}
+
+func (d detectState) isValidCSV() bool {
+	if d.invalid {
+		return false
 	}
 
 	var fieldCount int
-	for _, fields := range recordFields {
+	for _, fields := range d.recordFields {
 		if fields > 0 {
 			fieldCount = fields
 			break
@@ -92,14 +433,14 @@ loop:
 	}
 
 	var badFieldCount bool
-	for _, fields := range recordFields {
+	for _, fields := range d.recordFields {
 		if fields != fieldCount {
 			badFieldCount = true
 			break
 		}
 	}
 
-	return !badFieldCount && fieldCount > 1 && lineIdx > 0
+	return !badFieldCount && fieldCount > 1 && d.csvLineIdx > 1
 }
 
 func prepSvReader(in []byte, limit uint32) io.Reader {
--- internal/csv/detect_test.go
+++ internal/csv/detect_test.go
@@ -1,18 +1,53 @@
 package csv
 
 import (
+	"bytes"
+	"encoding/csv"
+	"errors"
+	"github.com/gabriel-vasile/mimetype/internal/util"
 	"io"
 	"reflect"
+	"strings"
 	"testing"
 )
 
+// svStdlib was the previous function used for CSV/TSV detection. It is currently
+// used to test the correctness of CSV detection.
+func svStdlib(in []byte, comma rune, limit uint32) bool {
+	r := svStdlibReader(in, comma, limit)
+
+	lines := 0
+	for {
+		_, err := r.Read()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return false
+		}
+		lines++
+	}
+
+	return r.FieldsPerRecord > 1 && lines > 1
+}
+
+func svStdlibReader(in []byte, comma rune, limit uint32) *csv.Reader {
+	r := csv.NewReader(bytes.NewReader(util.DropLastLine(in, limit)))
+	r.Comma = comma
+	r.ReuseRecord = true
+	r.LazyQuotes = true
+	r.Comment = '#'
+	return r
+}
+
 func TestDetect(t *testing.T) {
 	tests := []struct {
-		name      string
-		delimiter byte
-		input     string
-		limit     uint32
-		want      bool
+		name              string
+		delimiter         byte
+		input             string
+		limit             uint32
+		want              bool
+		skipStdlibCompare bool
 	}{
 		{
 			name:      "empty",
@@ -27,9 +62,45 @@ func TestDetect(t *testing.T) {
 			want:      false,
 		},
 		{
+			name:      "csv single line with newlines",
+			delimiter: ',',
+			input:     "a,b,c\n\n",
+			want:      false,
+		},
+		{
+			name:      "csv single line with (windows line ending + comment)",
+			input:     ",\r\n#,",
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name:      "csv functionally single line (has empty comment as top line)",
+			delimiter: ',',
+			input:     "#\na,b,c",
+			want:      false,
+		},
+		{
+			name:      "single line csv with following comment line",
+			delimiter: ',',
+			input:     "1,2\n#something\n\n",
+			want:      false,
+		},
+		{
 			name:      "csv multiple lines",
 			delimiter: ',',
-			input:     "a,b,c\n1,2,3",
+			input:     "a,b,c,d,e,f,g,h\n1,2,3,4,5,6,7,8",
+			want:      true,
+		},
+		{
+			name:      "csv multiple lines (windows line endings)",
+			input:     "a,b,c\r\n1,2,3",
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name:      "multi line csv with last line as comment",
+			delimiter: ',',
+			input:     "1,2\n1,2\n#something",
 			want:      true,
 		},
 		{
@@ -50,7 +121,9 @@ func TestDetect(t *testing.T) {
 			delimiter: ',',
 			input:     "a,b,c\n1,2,3\n4,5,6",
 			limit:     1,
-			want:      false,
+			want:      false, // we're only allowed to read a single byte, this could never be a CSV
+			// the previous limit value was not being honored (all bytes were read from the reader instead of the limit)
+			skipStdlibCompare: true,
 		},
 		{
 			name:      "csv 2 line with incomplete last line",
@@ -68,20 +141,40 @@ func TestDetect(t *testing.T) {
 		{
 			name:      "within quotes",
 			delimiter: ',',
-			input:     "\"a,b,c\n1,2,3\n4,5,6\"",
-			want:      false,
+			input: `"a,b,c
+1,2,3
+4,5,6"`,
+			want: false,
 		},
 		{
 			name:      "partial quotes",
 			delimiter: ',',
-			input:     "\"a,b,c\n1,2,3\n4,5,6",
-			want:      false,
+			input: `"a,b,c
+1,2,3
+4,5,6`,
+			want: false,
 		},
 		{
 			name:      "has quotes",
 			delimiter: ',',
-			input:     "\"a\",\"b\",\"c\"\n1,\",\"2,3\n\"4\",5,6",
+			input: `"a","b","c"
+1,",2",3
+"4",5,6`,
+			want: true,
+		},
+		{
+			name: "all quotes missing newline",
+			input: `"a","b","c"
+"1","2","3"`,
+			delimiter: ',',
 			want:      true,
+		},
+		{
+			name:      "has improper quote encapsulation",
+			delimiter: ',',
+			input: `1,2,3
+1,","2,3`,
+			want: false,
 		},
 		{
 			name:      "comma within quotes",
@@ -119,14 +212,411 @@ func TestDetect(t *testing.T) {
 			input:     "#comment\na,b,c\n\n\n#something else\n1,2,3",
 			want:      true,
 		},
+		{
+			name:      "resilient to empty lines",
+			delimiter: ',',
+			input:     "\n0,\n\n0,\n\n",
+			want:      true,
+		},
+		{
+			name:      "single row with one field",
+			delimiter: ',',
+			input:     "0,",
+			want:      false,
+		},
+		{
+			name:      "space prefix invalidates format",
+			delimiter: ',',
+			input:     " #\na,b,c\na,b,c",
+			want:      false,
+		},
+		{
+			name:      "comment with quotes",
+			delimiter: ',',
+			input: `#comment with "quotes"
+a,b,c
+#some"thing" else "oops this is imbalanced
+1,2,3`,
+			want: true,
+		},
+		{
+			name:      "a quote counts as content",
+			delimiter: ',',
+			input: `,
+"`,
+			want: false,
+		},
+		{
+			name: "unbalanced quotes within field",
+			input: `,,"""
+#0`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "last non-empty field in first row has single unbalanced quote with prefix",
+			input: `,,aa00110"
+,,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "last empty field with balanced quotes",
+			input: `,,"",
+,,,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "last field with single unbalanced quote",
+			input: `,,,"
+,,,
+,,,`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "first field in first row has unbalanced quote",
+			input: `",
+,`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "first field in first row has unbalanced quote with field prefix",
+			input: `0",
+,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "middle field in first row has unbalanced quote",
+			input: `,",
+,,`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "middle field in first row has unbalanced quote with prefix",
+			input: `#something
+#else
+,0",0
+,,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "last field with single unbalanced quote on last row",
+			input: `,
+,
+,"`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "last field with single unbalanced quote on last row with prefix",
+			input: `,
+,
+,0a"`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "last field with single unbalanced quote on middle row",
+			input: `,
+,"
+,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "last field with single unbalanced quote on middle row with field prefix",
+			input: `,
+,0a"
+,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "multiple empty rows",
+			input: `,
+,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "middle field with single unbalanced quote",
+			input: `,",
+,,`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "imbalanced quotes across lines",
+			input: `,
+",","
+""something"","
+,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "ignore escaped quotes",
+			input: `0,0,0
+0,"something ""else"" is happening",0
+0,0,0`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "3 quotes is not an escape",
+			input: `0,0,0
+0,""",0
+0,0,0`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "infer starting quote",
+			input: `0,0,0
+0,0""",0
+0,0,0`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "literal starting quote",
+			input: `0,0,0
+0,"0""",0
+0,0,0`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "infer starting quote with whitespace",
+			input: `0  , 0, 0  
+0,   0""",0
+0,0,0`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name:      "handle null characters as content",
+			input:     ",,\n\x00,,",
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name:      "comment character within inferred quote",
+			input:     ",\n0#,",
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name:      "allow carriage return as content",
+			input:     ",\n,\r0",
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name:      "stdlib edge case: drop trailing carriage return",
+			input:     ",\n,\n\r",
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name:      "dont drop multiple trailing carriage returns (they should be treated as a bad data row)",
+			input:     ",\n,\n\r\r",
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "inferred escaped quote within the middle of a field",
+			input: `,
+0"0,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "detect explicit quotes and escapes",
+			input: `""0","",",,,"
+0,",,,","`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "too many escapes",
+			input: `"""0","",",,,"
+0,",,,","`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "explicit quote start without explicit end (odd)",
+			input: `""""0,0,0
+0,0,0`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "explicit quote start without explicit end (even)",
+			input: `"""0,0,0
+0,0,0`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "several valid escapes",
+			// interpreted as:
+			// 0: []string{
+			//			"\"\"0",   // explicit quoted string with 2 escaped quotes (4 literal)... 5 quotes total
+			//			"\"\"",    // explicit quoted string with 2 escaped quotes (4 literal)... 6 quotes total
+			//			",,,",     // delimiter embedded within quotes in a single field
+			//		}
+			// 1: []string{
+			//			"0",      // implicit quote start and end
+			//			",\",,",  // explicit quoted string with embedded delimiter and one escaped quotes (2 literal)... 4 quotes total
+			//			"",       // implicit quote end
+			//			}
+			input: `""""0","""""",",,,"
+0,","",,","`,
+
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "explicit quote starts detect escapes and explicit end quotes",
+			input: `,,"0"0"
+,,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "explicit quote starts detect escapes and explicit end quotes (multi field)",
+			input: `,"0""0","0"0"
+,,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "inferred quote starts detect escapes and explicit end quotes",
+			input: `,0""0","0"0"
+,,`,
+			delimiter: ',',
+			want:      true,
+		},
+		{
+			name: "if a field starts a quoted it must explicitly be end quoted",
+			input: `,"0""0,
+,,`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "first line triple quotes are not",
+			input: `""",0
+0,0`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "last line with triple quotes and line ending are not ok",
+			input: `1,2
+""",0
+`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "last line with triple quotes and no line ending are not ok",
+			input: `1,2
+""",0`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "last line with triple quotes, last field empty, and no line ending are not ok",
+			input: `,
+""",`,
+			delimiter: ',',
+			want:      false,
+		},
+		{
+			name: "erg",
+			input: `1,
+2,"""",3`,
+			delimiter: ',',
+			want:      false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Detect([]byte(tt.input), tt.delimiter, tt.limit); got != tt.want {
-				t.Errorf("Csv() = %v, want %v", got, tt.want)
+			got := Detect([]byte(tt.input), tt.delimiter, tt.limit)
+
+			if got != tt.want {
+				t.Errorf("Detect(): got %v, want %v", got, tt.want)
 			}
+
+			if tt.skipStdlibCompare {
+				return
+			}
+
+			stdlib := svStdlib([]byte(tt.input), rune(tt.delimiter), tt.limit)
+
+			if got != stdlib {
+				t.Errorf("Detect(): got %v, stdlib %v", got, stdlib)
+				if stdlib {
+					reader := svStdlibReader([]byte(tt.input), rune(tt.delimiter), tt.limit)
+					records, err := reader.ReadAll()
+					if err != nil {
+						t.Fatalf("unable to get std records: %v", err)
+					}
+					for i, record := range records {
+						t.Logf("record %d: %#v", i+1, record)
+					}
+				}
+			}
+
 		})
 	}
+}
+
+func FuzzDetect(f *testing.F) {
+	samples := []string{
+		"a,b,c\n1,2,3",     // simple csv
+		"a,b,c\r\n1,2,3",   // with \r\n line ending
+		"a,b,c\n#c\n1,2,3", // with comment
+		"æ,ø,å\n1,2,3",     // utf-8
+
+		`"a","b","c"
+"1","2","3"`, // quotes
+
+		`a,b,c
+#"c"
+1,2,3`, // quoted comment
+	}
+
+	for _, s := range samples {
+		f.Add([]byte(s))
+	}
+
+	f.Fuzz(func(t *testing.T, d []byte) {
+		//if strings.Contains(string(d), "\"\"") {
+		//	return
+		//}
+
+		if strings.Count(string(d), "\n") >= svLineLimit {
+			// we no longer check the entire CSV
+			return
+		}
+
+		prev := svStdlib(d, ',', 0)
+		curr := Detect(d, ',', 0)
+		if prev != curr {
+			t.Errorf("curr detector does not match prev:\ncurr: %t, stdlib: %t, input: %s",
+				curr, prev, string(d))
+		}
+	})
 }
 
 func Test_prepSvReader(t *testing.T) {
